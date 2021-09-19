e3fa834678be307494393cb4d9fd37df33b33cb7
Fatih ÇELİK <84670981+kutbey@users.noreply.github.com>
2021-09-19 21:49:35 +0300

    change class name and cleanup code

--- ultimate.py
+++ ultimate.py
@@ -1,10 +1,17 @@
-class ultimateStr:
-    def __init__(self, word: str, lowerLetters: str = None, upperLetters: str = None):
+class uStr:
+    def __init__(self, word: str, lowerLetters: str | list | tuple = None, upperLetters: str | list | tuple = None):
         self.word = word
-        if not lowerLetters:
+        if not lowerLetters and not upperLetters:
             self.lowerLetters = "abcçdefgğhıijklmnoöprsştuüvyz"
-        if not upperLetters:
             self.upperLetters = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ"
+        else:
+            lo = list(lowerLetters.replace(" ", "").strip())
+            up = list(upperLetters.replace(" ", "").strip())
+            if len(up) != len(lo):
+                raise Exception("The alphabet in different lengths!")
+            else:
+                self.lowerLetters = "".join(lo)
+                self.upperLetters = "".join(up)
 
     def make_trans_upper(self) -> dict:
         changeTable = str.maketrans(self.lowerLetters, self.upperLetters)
@@ -24,21 +31,24 @@ class ultimateStr:
         return f"{self.lower()[0].translate(self.make_trans_upper())}{self.lower()[1:]}"
 
     def title(self) -> str:
-        firstChrIndex = [x + 1 for x in range(len(self.word)) if
-                         self.word[x] in (" ", "\n", ".", "!", "?", ":") and (x + 1 != len(self.word))]
+        firstChrIndex = [x + 1 for x in range(len(self.word)) if self.word[x] in (" ", "\n", ".", "!", "?", ":") \
+                         and (x + 1 != len(self.word))]
         firstChrIndex.insert(0, 0)
-        copyWord = list(self.word)
+        lowerCaseList = []
+        for x in list(self.word):
+            lowerCaseList.append(x.translate(self.make_trans_lower()))
+
         for y in firstChrIndex:
-            copyWord[y] = copyWord[y].translate(self.make_trans_upper())
-        return "".join(copyWord)
+            lowerCaseList[y] = lowerCaseList[y].translate(self.make_trans_upper())
+        return "".join(lowerCaseList)
 
     def forSorted(self) -> tuple:
         cleanWords = ""
-        for l in self.lower().replace(" ", ""):
+        for l in self.lower().strip().replace(" ", ""):
             if l in self.lowerLetters:
                 cleanWords += l
         letterOrder = {i: self.lowerLetters.index(i) for i in self.lowerLetters}
-        sorX = [letterOrder.get(cleanWords[i]) for i in range(len(cleanWords))]  # to use sorted method
+        sorX = [(letterOrder.get(cleanWords[i])) for i in range(len(cleanWords))]  # to use sorted method
         return tuple(sorX)
 
     def __str__(self):
@@ -47,16 +57,4 @@ class ultimateStr:
     def __repr__(self):
         return f"ultimateStr({self.word})"
 
-print(ultimateStr("FAtih Çelik Isparta").upper())
-print(ultimateStr("FAtih Çelik Isparta").lower())
-print(ultimateStr("FAtih Çelik Isparta").title())
-print(ultimateStr("FAtih Çelik Isparta").capitalize())
-print(ultimateStr("FAtih Çelik Isparta").forSorted())
 
-# OUTPUTLAR
-
-# FATİH ÇELİK ISPARTA
-# fatih çelik ısparta
-# FAtih Çelik Isparta
-# Fatih çelik ısparta
-# (6, 0, 23, 11, 9, 3, 5, 14, 11, 13, 10, 21, 19, 0, 20, 23, 0)
